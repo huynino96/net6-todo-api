855f870e34e7d899bfeaeb011e57dfd53bffb283
Huy Ho <huynino0496@gmail.com>
2022-08-25 09:16:20 +0700

    Added ToDo Model and Controller

---
+++ ToDoApi/ToDoApi/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ ToDoApi/ToDoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ToDoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
+
