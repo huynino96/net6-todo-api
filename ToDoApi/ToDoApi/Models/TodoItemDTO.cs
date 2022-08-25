62c9c056785d571de644ba54fe8e5128ee1c58a1
Huy Ho <huynino0496@gmail.com>
2022-08-25 09:35:55 +0700

    Added DTO to prvevent over-posting

---
+++ ToDoApi/ToDoApi/Models/TodoItemDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ToDoApi.DTO
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
+
